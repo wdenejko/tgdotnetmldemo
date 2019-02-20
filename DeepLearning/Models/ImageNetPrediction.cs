70e1427c145da975f2efcfd87dadd77b26156395
Wojtek Denejko <wojciech.denejko@gmail.com>
2019-02-20 17:20:35 +0100

    DeepLearning

---
+++ DeepLearning/Models/ImageNetPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace DeepLearning.Models
+{
+    public class ImageNetPrediction
+    {
+        [ColumnName(TFModelScorer.InceptionSettings.outputTensorName)]
+        public float[] PredictedLabels;
+    }
+}
--- DeepLearning/Program.cs
+++ DeepLearning/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace DeepLearning
 {
@@ -6,7 +7,28 @@ namespace DeepLearning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var assetsPath = ModelHelpers.GetAssetsPath(@"..\..\..\Data");
+
+            var tagsTsv = Path.Combine(assetsPath, "inputs", "images", "tags.tsv");
+            var imagesFolder = Path.Combine(assetsPath, "inputs", "images");
+            var inceptionPb = Path.Combine(assetsPath, "inputs", "inception", "tensorflow_inception_graph.pb");
+            var labelsTxt = Path.Combine(assetsPath, "inputs", "inception", "imagenet_comp_graph_label_strings.txt");
+
+            var customInceptionPb = Path.Combine(assetsPath, "inputs", "inception_custom", "model_tf.pb");
+            var customLabelsTxt = Path.Combine(assetsPath, "inputs", "inception_custom", "labels.txt");
+
+            try
+            {
+                var modelScorer = new TFModelScorer(tagsTsv, imagesFolder, inceptionPb, labelsTxt);
+                modelScorer.Score();
+
+            }
+            catch (Exception ex)
+            {
+                ConsoleHelpers.ConsoleWriteException(ex.Message);
+            }
+
+            ConsoleHelpers.ConsolePressAnyKey();
         }
     }
 }
